364c6531ac71b1f02221488481adb01cb80868ec
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2021-03-03 08:08:17 +0200

    add parallel

--- count_test.go
+++ count_test.go
@@ -4,8 +4,13 @@ import (
 	"testing"
 )
 
-func BenchmarkFoo(b *testing.B) {
+func BenchmarkNotParallel(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		GetBodyLens()
+	}
+}
+func BenchmarkParallel(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		GetBodyLens2()
 	}
 }
--- url_count.go
+++ url_count.go
@@ -20,18 +20,44 @@ func getBodyLen(url string) (int, error) {
 	log.Printf("Getting body for %s", url)
 	return utf8.RuneCountInString(string(body)), nil
 }
+func getBodyLen2(url string, ch chan struct{}) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("Error")
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	log.Printf("Getting body for %s", url)
+	charsLen := utf8.RuneCountInString(string(body))
+	log.Printf("chars: %d", charsLen)
+	ch <- struct{}{}
+}
 
 func GetBodyLens() {
 	urls := []string{"https://www.google.com", "https://www.nytimes.com",
 		"https://www.trello.com", "https://mytzedakah.com/create-fund/1",
-		"https://www.adobe.com"}
-
+		"https://www.adobe.com", "https://www.craigslist.com"}
 	for _, url := range urls {
 		len, _ := getBodyLen(url)
 		log.Printf("characters: %d", len)
 	}
 }
-func main() {
-	GetBodyLens()
 
+func GetBodyLens2() {
+
+	urls := []string{"https://www.google.com", "https://www.nytimes.com",
+		"https://www.trello.com", "https://mytzedakah.com/create-fund/1",
+		"https://www.adobe.com", "https://www.craigslist.com"}
+	len := make(chan struct{})
+
+	for _, url := range urls {
+		go getBodyLen2(url, len)
+	}
+	for range urls {
+		<-len
+	}
+
+}
+func main() {
+	GetBodyLens2()
 }
